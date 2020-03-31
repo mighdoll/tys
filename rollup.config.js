acebe879c751f03f766880cad9704946ddb2cdf3
lee <lee@underneath.ca>
2020-03-30 22:46:52 -0700

    update configs and create src dir

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,13 @@
 import typescript2 from "rollup-plugin-typescript2";
-// import { terser as rollupTerser } from "rollup-plugin-terser";
 
 export default {
-  input: "tys.ts",
+  input: "src/tys.ts",
   output: {
     dir: "dist",
     format: "cjs",
     sourcemap: true,
     banner: "#!/usr/bin/env node"
   },
-  plugins: [
-    typescript2({
-      outDir: "dist"
-    }),
-    // rollupTerser() // minify step. Comment this out to review bundle contents
-  ],
-  external: ["fs", "path", "events", "child_process", "config-file-ts", "glob"],
+  plugins: [typescript2()],
+  external: ["fs", "path", "events", "child_process", "config-file-ts", "glob"]
 };
