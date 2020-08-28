c75fbc357e566567d765a582612d1a45cb805f01
lee <lee@underneath.ca>
2020-08-28 15:16:03 -0700

    rm hard to maintain test

--- test/scriptys.test.ts
+++ test/scriptys.test.ts
@@ -2,12 +2,9 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import "chai/register-should";
-import { defaultOutDir, symLinkForce } from "config-file-ts";
-import fs from "fs";
-import path from "path";
+import { defaultOutDir } from "config-file-ts";
 import rimraf from "rimraf";
-import tmp from "tmp";
-import { bufferedRun, run } from "../src/execUtil";
+import { run } from "../src/execUtil";
 
 chai.use(chaiAsPromised);
 
@@ -41,40 +38,6 @@ test("recursively run tys on tys launcher", () => {
   return result.should.eventually.equal(0);
 });
 
-test.skip("gulptys --help", async () => {
-  const { dir, gulptys } = createGulptysLink();
-  createNodeModules(dir);
-  const result = bufferedRun(`cd test; ${gulptys} --help`);
-  const done = result.then((execResult) => {
-    execResult.result.should.equal(0);
-    execResult.stdout.should.contain("Usage: gulp");
-    return execResult;
-  })
-  .finally(() => rimraf.sync(dir));
-  return done;
-});
-
-interface GulptysLink {
-  dir: string;
-  gulptys: string;
-}
-
-function createNodeModules(dir: string) {
-  const node_modules = path.join(dir, "node_modules", "tys");
-  fs.mkdirSync(node_modules, { recursive: true });
-  const target = path.join(node_modules, "tys.d.ts");
-  fs.copyFileSync("/home/lee/tys/dist/tys.d.ts", target);
-}
-
-function createGulptysLink(): GulptysLink {
-  const dir = tmp.dirSync().name;
-  const gulptys = path.join(dir, "gulptys");
-  const tysPath = path.resolve("dist/tys.js");
-  symLinkForce(tysPath, gulptys);
-
-  fs.chmodSync(gulptys, 0o775);
-  return { dir, gulptys };
-}
 
 function clearCache(...tsFiles: string[]): void {
   for (const tsFile of tsFiles) {
